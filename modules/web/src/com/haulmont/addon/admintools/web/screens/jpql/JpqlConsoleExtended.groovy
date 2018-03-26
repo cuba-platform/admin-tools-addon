dd691a3b4dc7dee96efa22a7bd577f9ae1105b43
Evgeniy Loginov <loginov@haulmont.com>
2018-03-26 16:27:27 +0400

    bat-219 add simple autocomplition for JPQL console

--- modules/web/src/com/haulmont/addon/admintools/web/screens/jpql/JpqlConsoleExtended.groovy
+++ modules/web/src/com/haulmont/addon/admintools/web/screens/jpql/JpqlConsoleExtended.groovy
@@ -1,13 +1,21 @@
 package com.haulmont.addon.admintools.web.screens.jpql
 
-import com.haulmont.bali.util.ParamsMap
+import com.haulmont.chile.core.model.MetaClass
+import com.haulmont.cuba.core.global.Metadata
+import com.haulmont.cuba.gui.components.SourceCodeEditor
+import com.haulmont.cuba.gui.components.autocomplete.AutoCompleteSupport
+import com.haulmont.cuba.gui.components.autocomplete.Suggestion
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory
+import com.haulmont.cuba.web.gui.components.WebSourceCodeEditor
 import de.diedavids.cuba.runtimediagnose.db.DbDiagnoseService
 import de.diedavids.cuba.runtimediagnose.diagnose.DiagnoseType
 import de.diedavids.cuba.runtimediagnose.web.screens.console.ConsoleFrame
 import de.diedavids.cuba.runtimediagnose.web.screens.jpql.JpqlConsole
 
 import javax.inject.Inject
+import java.util.stream.Collector
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
 class JpqlConsoleExtended extends JpqlConsole {
 
@@ -17,10 +25,32 @@ class JpqlConsoleExtended extends JpqlConsole {
     ConsoleFrame consoleFrame
     @Inject
     ComponentsFactory componentsFactory
+    @Inject
+    Metadata metadata
 
     @Override
     void init(Map<String, Object> params) {
         params.put('diagnoseType', DiagnoseType.JPQL)
-        openFrame(consoleFrame, 'console-frame', params)
+        def frame = openFrame(consoleFrame, 'console-frame', params)
+
+        WebSourceCodeEditor console = frame.getComponent("console") as WebSourceCodeEditor
+        console.setSuggester({ source, text, cursorPosition ->
+            getHint(console, text, cursorPosition)
+        })
+    }
+
+    protected List<Suggestion> getHint(SourceCodeEditor sender, String text, int senderCursorPosition) {
+        Collection<MetaClass> persistentClasses = metadata.getTools().getAllPersistentMetaClasses()
+        Collection<MetaClass> embeddableClasses = metadata.getTools().getAllEmbeddableMetaClasses()
+
+        int startPosition = senderCursorPosition - 2 < 0 ? 0 : senderCursorPosition - 2
+
+        Stream.concat(persistentClasses.stream(), embeddableClasses.stream())
+                .map({ metaClass ->
+                    String className = metaClass.getName()
+                    return new Suggestion(sender as AutoCompleteSupport, className, className, '', startPosition, senderCursorPosition)
+                })
+                .collect(Collectors.toList() as Collector<? super Object, Object, Object>) as List<Suggestion>
+
     }
 }
