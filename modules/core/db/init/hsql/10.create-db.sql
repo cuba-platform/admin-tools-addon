2b6534f8ac1e9104a532aa930adc10c48514c22f
Evgeniy Loginov <loginov@haulmont.com>
2018-03-30 15:35:35 +0400

    add sql scripts

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -1,1 +1,21 @@
-
+-- begin CUBAAT_SSH_CREDENTIAL
+create table CUBAAT_SSH_CREDENTIAL (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    HOSTNAME varchar(255) not null,
+    SESSION_NAME varchar(255),
+    IS_FOR_EVERYONE_USERS boolean,
+    PORT integer not null,
+    LOGIN varchar(255) not null,
+    PRIVATE_KEY_ID varchar(36),
+    --
+    primary key (ID)
+)^
+-- end CUBAAT_SSH_CREDENTIAL
